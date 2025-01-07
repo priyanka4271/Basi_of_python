9abefcca9ecaf89a33788918b3319fb665d9c781
priyanka4271 <ps9673932@gmail.com>
2025-01-07 12:24:45 +0530

    add new files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Basi_of_python
 this is my first repository
+<br>
+<h5> hello!<  my name is priyanka sharma <h5>git 
---
+++ factorial.py
@@ -0,0 +1,9 @@
+num=(int(input(" hello enter a number")))
+fact=1
+for i in range(1,num+1):
+      fact=fact*i
+
+
+
+print(fact)
+
