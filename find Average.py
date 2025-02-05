b4382710debfac1e058c470ff40731576f979d72
priyanka4271 <ps9673932@gmail.com>
2025-02-05 21:17:22 +0530

    add new file

---
+++ find Average.py
@@ -0,0 +1,12 @@
+aa =[-1,-2,11,25,0,-5]
+sum=0
+count=0
+for i in aa:
+    if i>0:
+        sum= sum+i
+        count=count+1
+if count>0:
+    avg=sum/count
+
+print("sum of postive number",sum)
+print("Average of postive number",avg)
