67c1033d29c038edcdbbec6c74f5d1e1e9264792
priyanka4271 <ps9673932@gmail.com>
2025-02-07 21:51:06 +0530

    create a new file

---
+++ Average.py
@@ -0,0 +1,11 @@
+for i in aa:
+    if i>0:
+        sum= sum+i
+        count=count+1
+if count>0:
+    avg=sum/count
+    #welcome to my new bharti vlog
+
+print("sum of postive number",sum)
+print("Average of postive number",avg)
+print("hello world")
